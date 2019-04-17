286e768b2a394faca6d0bd808573c484f5ba6738
Maxime Lovino <maximelovino@gmail.com>
2019-04-17 21:20:08 +0200

    Actually, with Java 11 it's faster in parallel, added eager parallel version

--- src/Ex8/NumericalIntegration.scala
+++ src/Ex8/NumericalIntegration.scala
@@ -6,45 +6,54 @@ import utils._
 object NumericalIntegration extends App {
   def trapezArea(y0: Double, y1: Double, intervalSize: Double) = ((y0 + y1) * intervalSize) / 2
 
-  def integrate(a: Double, b: Double, nIntervals: Int, f: Double => Double): Double = {
+  def integrate(a: Double, b: Double, nIntervals: Double, f: Double => Double): Double = {
     val intervalSize = (b - a) / nIntervals
 
     val range = a.until(b).by(intervalSize)
-    range.foldLeft(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
+    range.fold(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
   }
 
-  def integrateLazy(a: Double, b: Double, nIntervals: Int, f: Double => Double): Double = {
+  def integrateLazy(a: Double, b: Double, nIntervals: Double, f: Double => Double): Double = {
     val intervalSize = (b - a) / nIntervals
 
     val range = a.until(b).by(intervalSize).view
-    range.foldLeft(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
+    range.fold(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
   }
 
-  def integrateLazyPar(a: Double, b: Double, nIntervals: Int, f: Double => Double): Double = {
+  def integratePar(a: Double, b: Double, nIntervals: Double, f: Double => Double): Double = {
+    val intervalSize = (b - a) / nIntervals
+
+    val range = a.until(b).by(intervalSize).par
+    range.fold(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
+  }
+
+  def integrateLazyPar(a: Double, b: Double, nIntervals: Double, f: Double => Double): Double = {
     val intervalSize = (b - a) / nIntervals
 
     val range = a.until(b).by(intervalSize).view.par
-    range.foldLeft(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
+    range.fold(0.0)((acc, current) => acc + trapezArea(f(current), f(current + intervalSize), intervalSize))
   }
 
 
-  val INTERVALS = Math.pow(20, 6).toInt
+  val INTERVALS = Math.pow(20, 6)
   val sin = (x: Double) => Math.sin(x)
   val cos = (x: Double) => Math.cos(x)
   val f = sin compose cos
   println(integrate(0, 1, 10000, f))
-  val timeIntegrationLazyPar = time {
-    integrateLazyPar(0, 1, INTERVALS, f)
+  val timeIntegration = time {
+    integrate(0, 1, INTERVALS, f)
   }
   val timeIntegrationLazy = time {
     integrateLazy(0, 1, INTERVALS, f)
   }
-  val timeIntegration = time {
-    integrate(0, 1, INTERVALS, f)
+  val timeIntegrationPar = time {
+    integratePar(0, 1, INTERVALS, f)
+  }
+  val timeIntegrationLazyPar = time {
+    integrateLazyPar(0, 1, INTERVALS, f)
   }
   println(s"Standard: $timeIntegration ms")
-
   println(s"Lazy: $timeIntegrationLazy ms")
-
+  println(s"Parallel: $timeIntegrationPar ms")
   println(s"Lazy parallel: $timeIntegrationLazyPar ms")
 }
